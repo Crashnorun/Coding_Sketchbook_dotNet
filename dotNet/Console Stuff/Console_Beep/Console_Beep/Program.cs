80e4c0ce4aa6f9c9df650691f273ea7ece1e367d
crashnorun <charlie.portelli@gmail.com>
2020-12-17 15:18:25 -0500

    console beep testing

---
+++ dotNet/Console Stuff/Console_Beep/Console_Beep/Program.cs
@@ -0,0 +1,138 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Runtime.InteropServices;
+using System.Threading;
+
+namespace Console_Beep
+{
+    class Program
+    {
+
+        [DllImport("kernel32.dll", SetLastError = true)]
+        static extern bool Beep(uint dwFreq, uint dwDuration);
+
+        static void Main(string[] args)
+        {
+
+              Console.WriteLine("Testing PC Speaker...");
+              for(int i = 32700; i >200; i-=10)
+              {
+                  Console.WriteLine(i.ToString());
+
+                  //Beep(i, 5);
+                  Console.Beep(32767, 10000);
+                  //System.Threading.Thread.Sleep(1000);
+                  Console.SetCursorPosition(0, Console.CursorTop - 1);
+                  ClearCurrentConsoleLine();
+              }
+              Console.WriteLine("Testing Complete");
+
+
+            /*    Note[] Mary =
+            {
+            new Note(Tone.B, Duration.QUARTER),
+            new Note(Tone.A, Duration.QUARTER),
+            new Note(Tone.GbelowC, Duration.QUARTER),
+            new Note(Tone.A, Duration.QUARTER),
+            new Note(Tone.B, Duration.QUARTER),
+            new Note(Tone.B, Duration.QUARTER),
+            new Note(Tone.B, Duration.HALF),
+            new Note(Tone.A, Duration.QUARTER),
+            new Note(Tone.A, Duration.QUARTER),
+            new Note(Tone.A, Duration.HALF),
+            new Note(Tone.B, Duration.QUARTER),
+            new Note(Tone.D, Duration.QUARTER),
+            new Note(Tone.D, Duration.HALF)
+            };
+                // Play the song
+                Play(Mary);*/
+
+
+
+        }
+
+
+        public static void ClearCurrentConsoleLine()
+        {
+            int currentLineCursor = Console.CursorTop;
+            Console.SetCursorPosition(0, Console.CursorTop);
+            Console.Write(new string(' ', Console.WindowWidth));
+            Console.SetCursorPosition(0, currentLineCursor);
+        }
+
+
+        // Play the notes in a song.
+        protected static void Play(Note[] tune)
+        {
+            foreach (Note n in tune)
+            {
+                if (n.NoteTone == Tone.REST)
+                    Thread.Sleep((int)n.NoteDuration);
+                else
+                    Console.Beep((int)n.NoteTone, (int)n.NoteDuration);
+            }
+        }
+
+
+        // Define the frequencies of notes in an octave, as well as
+        // silence (rest).
+        protected enum Tone
+        {
+            REST = 0,
+            GbelowC = 196,
+            A = 220,
+            Asharp = 233,
+            B = 247,
+            C = 262,
+            Csharp = 277,
+            D = 294,
+            Dsharp = 311,
+            E = 330,
+            F = 349,
+            Fsharp = 370,
+            G = 392,
+            Gsharp = 415,
+        }
+
+        // Define the duration of a note in units of milliseconds.
+        protected enum Duration
+        {
+            WHOLE = 1600,
+            HALF = WHOLE / 2,
+            QUARTER = HALF / 2,
+            EIGHTH = QUARTER / 2,
+            SIXTEENTH = EIGHTH / 2,
+        }
+
+        // Define a note as a frequency (tone) and the amount of
+        // time (duration) the note plays.
+        protected struct Note
+        {
+            Tone toneVal;
+            Duration durVal;
+
+            // Define a constructor to create a specific note.
+            public Note(Tone frequency, Duration time)
+            {
+                toneVal = frequency;
+                durVal = time;
+            }
+
+            // Define properties to return the note's tone and duration.
+            public Tone NoteTone { get { return toneVal; } }
+            public Duration NoteDuration { get { return durVal; } }
+        }
+
+
+
+
+    }
+
+
+
+
+
+}
