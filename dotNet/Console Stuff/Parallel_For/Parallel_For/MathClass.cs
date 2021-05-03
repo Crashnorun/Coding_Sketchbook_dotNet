033c4299e2341a31021c17d9f97d74cad1a92d41
CharlieP <charlie.portelli@gmail.com>
2021-05-03 10:53:11 -0400

    parallel loop testing

---
+++ dotNet/Console Stuff/Parallel_For/Parallel_For/MathClass.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Parallel_For
+{
+    public class MathClass
+    {
+
+
+        public List<int> AddedNumbers { get; set; }
+
+        public List<int> SubtractedNumbers { get; set; }
+
+        public List<double> DividedNumbers { get; set; }
+
+
+
+    }
+}
--- dotNet/Console Stuff/Parallel_For/Parallel_For/Program.cs
+++ dotNet/Console Stuff/Parallel_For/Parallel_For/Program.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Reflection;
+using System.Diagnostics;
 
 namespace Parallel_For
 {
@@ -19,49 +20,50 @@ namespace Parallel_For
             List<int> newNumsFor = new List<int>();
             List<int> newNumsOld = new List<int>();
 
-            for (int i = 0; i < 10000; i++)
+            // create list of ints
+            for (int i = 0; i < 10000000; i++)
             {
                 nums.Add(i);
-                newNumsOld.Add((int)Math.Pow(i, 2));
+                //newNumsOld.Add(i);
             }
             Console.WriteLine("newNumsOld " + nums.Count);
 
 
-           /* Parallel.ForEach(nums, num =>
-            {
-                Console.BackgroundColor = ConsoleColor.Blue;
-                newNumsForEach.Add((int)Math.Pow(num, 2));
-                //Console.WriteLine(num + " " + (int)Math.Pow(num, 2));
-            });
-            Console.WriteLine("newNumsForEach " + newNumsForEach.Count);*/
-
-
-          /*  Parallel.For(0, nums.Count, num =>
-            {
-                Console.BackgroundColor = ConsoleColor.Red;
-                newNumsFor.Add((int)Math.Pow(num, 2));
-                //Console.WriteLine(num + " " + (int)Math.Pow(num, 2));
-            });
-            Console.WriteLine("newNumsFor " + newNumsFor.Count);*/
-
-
-          /*  Console.BackgroundColor = ConsoleColor.Green;
-
-
-            for (int i = 0; i < nums.Count; i++)
-            {
-                Console.WriteLine(string.Format("{0} \t {1} \t {2}", newNumsOld[i], newNumsFor[i], newNumsForEach[i]));
-            }*/
+            List<int> AddedNums = new List<int>();
+            List<int> SubtractedNums = new List<int>();
+            List<double> DividedNums = new List<double>();
 
 
             List<MethodInfo> meths = new List<MethodInfo>();
             meths.Add(typeof(Program).GetMethod("Add"));
             meths.Add(typeof(Program).GetMethod("Subtract"));
+            meths.Add(typeof(Program).GetMethod("Divide"));
 
+            Stopwatch watch = new Stopwatch();
+            watch.Start();
             Parallel.ForEach(meths, meth =>
+             {
+                 var obj = meth.Invoke(null, new object[] { nums });
+                 if (obj.GetType() == typeof(List<double>)) DividedNums = (List<double>)obj;
+                 else
+                 {
+                     if (meth.Name == "Add") AddedNums = (List<int>)obj;
+                     else SubtractedNums = (List<int>)obj;
+                 }
+             });
+            watch.Stop();
+            Console.WriteLine(watch.ElapsedMilliseconds);
+
+            // https://docs.microsoft.com/en-us/dotnet/api/system.reflection.methodbase.invoke?view=net-5.0
+
+            Stopwatch watch2 = new Stopwatch();
+            watch2.Start();
+            foreach (MethodInfo meth in meths)
             {
-               var obj=  meth.Invoke(null, new object[] { nums });
-            });
+                var obj = meth.Invoke(null, new object[] { nums });
+            }
+            watch2.Stop();
+            Console.WriteLine(watch2.ElapsedMilliseconds);
 
             //Console.ReadKey();
         }
@@ -83,5 +85,45 @@ namespace Parallel_For
             foreach (int num in nums) newNums.Add(num + num);
             return newNums;
         }
+
+
+        public static List<double> Divide(List<int> nums)
+        {
+            Console.BackgroundColor = ConsoleColor.Red;
+            List<double> newNums = new List<double>();
+            foreach (int num in nums) newNums.Add(num / 2.0);
+            return newNums;
+        }
+
+
+        public static void CompareParallelLoops()
+        {
+            /* Parallel.ForEach(nums, num =>
+                       {
+                           Console.BackgroundColor = ConsoleColor.Blue;
+                           newNumsForEach.Add((int)Math.Pow(num, 2));
+                           //Console.WriteLine(num + " " + (int)Math.Pow(num, 2));
+                       });
+                       Console.WriteLine("newNumsForEach " + newNumsForEach.Count);*/
+
+
+            /*  Parallel.For(0, nums.Count, num =>
+              {
+                  Console.BackgroundColor = ConsoleColor.Red;
+                  newNumsFor.Add((int)Math.Pow(num, 2));
+                  //Console.WriteLine(num + " " + (int)Math.Pow(num, 2));
+              });
+              Console.WriteLine("newNumsFor " + newNumsFor.Count);*/
+
+
+            /*  Console.BackgroundColor = ConsoleColor.Green;
+
+
+              for (int i = 0; i < nums.Count; i++)
+              {
+                  Console.WriteLine(string.Format("{0} \t {1} \t {2}", newNumsOld[i], newNumsFor[i], newNumsForEach[i]));
+              }*/
+        }
+
     }
 }
