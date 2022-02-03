bd78abcf42fb31a1e0e18826708f5913a0c7b45f
CharlieP <charlie.portelli@gmail.com>
2022-02-03 09:34:22 -0500

    basic event example

---
+++ dotNet/Events_01/Events_01/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Events_01
+{
+    class Program
+    {
+        /// <summary>
+        /// Represents a method that will handel an event that has no event data
+        /// </summary>
+        public static EventHandler testEvent;
+
+
+        static void Main(string[] args)
+        {
+            // Subscribe to the event. 
+            // Add function TestEvent_OnKeyPressed to the event
+            testEvent += TestEvent.TestEvent_OnKeyPressed;
+
+            ConsoleKeyInfo key = Console.ReadKey();
+            RunEvent(key);
+
+            Console.ReadKey();
+        }
+
+
+        private static void RunEvent(ConsoleKeyInfo key)
+        {
+            if (key.Key == ConsoleKey.Spacebar)
+            {
+                // Invoke only if the function is not null.
+                //testEvent?.Invoke(MethodBase.GetCurrentMethod(), EventArgs.Empty);
+                testEvent?.Invoke(MethodBase.GetCurrentMethod(), EventArgs.Empty);
+            }
+        }
+    }
+}
+
+
+/*
+ * Reference: https://www.youtube.com/watch?v=OuZrhykVytg
+ */
---
+++ dotNet/Events_01/Events_01/TestEvent.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Events_01
+{
+    public class TestEvent
+    {
+        
+        public static void TestEvent_OnKeyPressed(object sender, EventArgs e)
+        {
+            MethodInfo info = sender as MethodInfo;
+
+            Console.WriteLine("This event was called by " + info.Name);
+        }
+    }
+}
