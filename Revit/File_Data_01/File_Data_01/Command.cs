a62353fae74ebedf64a8263b4eb197e06f5a9c70
CharlieP <charlie.portelli@gmail.com>
2022-02-05 14:39:02 -0500

    Update Command.cs

--- Revit/File_Data_01/File_Data_01/Command.cs
+++ Revit/File_Data_01/File_Data_01/Command.cs
@@ -26,7 +26,16 @@ namespace File_Data_01
     [Transaction(TransactionMode.Manual)]
     public class Command : IExternalCommand
     {
-        List<string> log = new List<string>();
+        #region ---- PROPERTIES ----
+
+        /// <summary>
+        /// Contains the CSV output saved to the file
+        /// </summary>
+        List<string> ModelData = new List<string>();
+
+        #endregion
+
+        #region ---- ENTRY POINT ----
 
         public Result Execute(ExternalCommandData commandData, ref string message, ElementSet elements)
         {
@@ -36,6 +45,7 @@ namespace File_Data_01
 
             if (uidoc == null)
             {
+                MessageBox.Show("No open Revit files found. Please open a file and try again", "Data Export", MessageBoxButtons.OK);
                 Debug.Print("Could not find any open Revit documents");
                 return Result.Cancelled;
             }
@@ -45,17 +55,18 @@ namespace File_Data_01
             FamilyInstanceData(doc);
             DetailLineData(doc);
             ViewData(doc);
-            // get levels
-            // get grids
+            LevelData(doc);
+            GridData(doc);
+            SheetData(doc);
 
             string filename = Path.GetFileName(doc.PathName);
             string folder = Path.GetDirectoryName(doc.PathName);
-            log.Add("File name: " + filename);
+            ModelData.Add("File name: " + filename);
             Debug.Print("FileName: " + filename);
 
-            if (log.Count > 0 && doc != null) File.WriteAllLines(folder + @"\"+ LogFileName(filename), log);
+            if (ModelData.Count > 0 && doc != null) File.WriteAllLines(folder + @"\" + LogFileName(filename), ModelData);
 
-            MessageBox.Show("Data Export Complete","Data Export",MessageBoxButtons.OK);
+            MessageBox.Show("Data Export Complete", "Data Export", MessageBoxButtons.OK);
             Debug.Print("---- DONE ----");
 
             /*  using (Transaction tx = new Transaction(doc))
@@ -67,7 +78,8 @@ namespace File_Data_01
             return Result.Succeeded;
         }
 
-
+        #region ---- METHODS ----
+        #endregion
         /// <summary>
         ///  find all family symbol data in the active revit document
         /// </summary>
@@ -79,39 +91,60 @@ namespace File_Data_01
             if (doc == null) return;
 
             //log.Add(Environment.NewLine);
-            log.Add("Family Symbol Data");
-            log.Add("Element ID, Family Name, Name, Creator, Last Changed, Owner, Family File Path");
+            ModelData.Add("Family Symbol Data");
+            ModelData.Add("Element ID, Family Name, Name, Creator, Last Changed, Owner, Family File Path, Created, Last Accessed");
 
             FilteredElementCollector col = new FilteredElementCollector(doc)
                  .OfClass(typeof(FamilySymbol));
-            
+
             foreach (Element e in col)
             {
                 FamilySymbol sym = e as FamilySymbol;
 
                 try
                 {
-                    Document famDoc = doc.EditFamily(sym.Family);
+                    Document famDoc;
+                    if (sym.GetType() == typeof(PanelType))
+                    {
+                        PanelType panelType = sym as PanelType;
+                        famDoc = panelType.Document;
+                    }
+                    else if (sym.GetType() == typeof(MullionType))
+                    {
+                        MullionType mullionType = sym as MullionType;
+                        famDoc = mullionType.Document;
+                    }
+                    else famDoc = doc.EditFamily(sym.Family);
 
                     WorksharingTooltipInfo info = WorksharingUtils.GetWorksharingTooltipInfo(doc, sym.Id);
 
                     if (famDoc == null)
                     {
-                        log.Add(sym.Id + "," + sym.FamilyName + "," + sym.Name + "," + info.Creator + "," + info.LastChangedBy + "," + info.Owner + "," + "NA");
+                        ModelData.Add(sym.Id + "," + sym.FamilyName + "," + sym.Name + "," + info.Creator + "," + info.LastChangedBy + "," + info.Owner + "," + "NA");
                         Debug.Print(string.Format("Element ID: {0}, FamilyName: {1}, Name: {2}, Creator: {3}, Owner: {4}, Last Edited By: {5}, FilePath: {6}",
                         sym.Id, sym.FamilyName, sym.Name, info.Creator, info.Owner, info.LastChangedBy, "NA"));
                     }
                     else
                     {
-                        log.Add(sym.Id + "," + sym.FamilyName + "," + sym.Name + "," + info.Creator + "," + info.LastChangedBy + "," + info.Owner + "," + famDoc.PathName);
-                        Debug.Print(string.Format("Element ID: {0}, FamilyName: {1}, Name: {2}, Creator: {3}, Last Edited By: {4}, Owner: {5}, FilePath: {6}",
-                        sym.Id, sym.FamilyName, sym.Name, info.Creator, info.Owner, info.LastChangedBy, famDoc.PathName));
+                        string created = string.Empty, edited = string.Empty;
+                        if (File.Exists(famDoc.PathName)) 
+                        {
+                            created = File.GetCreationTime(famDoc.PathName).Date.ToString();
+                            edited = File.GetLastAccessTime(famDoc.PathName).Date.ToString();
+                        }
+
+                        ModelData.Add(sym.Id + "," + sym.FamilyName + "," + sym.Name + "," +
+                            info.Creator + "," + info.LastChangedBy + "," + info.Owner + "," +
+                            famDoc.PathName + "," + created + "," + edited);
+
+                        Debug.Print(string.Format("Element ID: {0}, FamilyName: {1}, Name: {2}, Creator: {3}, Last Edited By: {4}, Owner: {5}, FilePath: {6}, Created: {7}, Last Accessed: {8}",
+                        sym.Id, sym.FamilyName, sym.Name, info.Creator, info.Owner, info.LastChangedBy, famDoc.PathName, created, edited));
                     }
                 }
                 catch (Exception ex)
                 {
-                    if (sym == null) log.Add(ex.Message);
-                    else log.Add(sym.Id + "," + ex.Message);
+                    if (sym == null) ModelData.Add(ex.Message);
+                    else ModelData.Add(sym.Id + "," + ex.Message);
                 }
                 //log.Add(Environment.NewLine);
             }
@@ -131,8 +164,8 @@ namespace File_Data_01
             Debug.Print("---- FAMILY INSTANCE DATA ----");
 
             //log.Add(Environment.NewLine);
-            log.Add("Family Instance Data");
-            log.Add("Element ID, Name, Creator, Last Changed, Owner");
+            ModelData.Add("Family Instance Data");
+            ModelData.Add("Element ID, Name, Creator, Last Changed, Owner");
 
             FilteredElementCollector col = new FilteredElementCollector(doc)
                 .OfClass(typeof(FamilyInstance));
@@ -145,15 +178,15 @@ namespace File_Data_01
                 {
                     WorksharingTooltipInfo info = WorksharingUtils.GetWorksharingTooltipInfo(doc, inst.Id);
 
-                    log.Add(inst.Id + "," + inst.Name + "," + info.Creator + "," + info.LastChangedBy + "," + info.Owner);
+                    ModelData.Add(inst.Id + "," + inst.Name + "," + info.Creator + "," + info.LastChangedBy + "," + info.Owner);
 
                     Debug.Print(string.Format("Element ID: {0}, Name: {1}, Creator: {2}, Last Edited By: {3}, Owner: {4}",
                     inst.Id, inst.Name, info.Creator, info.LastChangedBy, info.Owner));
                 }
                 catch (Exception ex)
                 {
-                    if (inst == null) log.Add(ex.Message);
-                    else log.Add(inst.Id + "," + ex.Message);
+                    if (inst == null) ModelData.Add(ex.Message);
+                    else ModelData.Add(inst.Id + "," + ex.Message);
                 }
                 //log.Add(Environment.NewLine);
             }
@@ -173,8 +206,8 @@ namespace File_Data_01
             Debug.Print("---- DETAIL LINE DATA ----");
 
             //log.Add(Environment.NewLine);
-            log.Add("Detail Line Data");
-            log.Add("Element ID, Name, Creator, Last Changed, Owner");
+            ModelData.Add("Detail Line Data");
+            ModelData.Add("Element ID, Name, Creator, Last Changed, Owner");
 
             FilteredElementCollector col = new FilteredElementCollector(doc)
                 .OfClass(typeof(CurveElement));
@@ -185,7 +218,7 @@ namespace File_Data_01
                 {
                     WorksharingTooltipInfo info = WorksharingUtils.GetWorksharingTooltipInfo(doc, e.Id);
 
-                    log.Add(e.Id + "," + e.Name + "," + info.Creator + "," + info.LastChangedBy + "," + info.Owner);
+                    ModelData.Add(e.Id + "," + e.Name + "," + info.Creator + "," + info.LastChangedBy + "," + info.Owner);
 
                     Debug.Print(string.Format("Element ID: {0}, Name: {1}, Creator: {2}, Last Edited By: {3}, Owner: {4}",
                     e.Id, e.Name, info.Creator, info.LastChangedBy, info.Owner));
@@ -193,8 +226,8 @@ namespace File_Data_01
                 }
                 catch (Exception ex)
                 {
-                    if (e == null) log.Add(ex.Message);
-                    else log.Add(e.Id + "," + ex.Message);
+                    if (e == null) ModelData.Add(ex.Message);
+                    else ModelData.Add(e.Id + "," + ex.Message);
                 }
                 //log.Add(Environment.NewLine);
             }
@@ -214,8 +247,8 @@ namespace File_Data_01
             Debug.Print("---- VIEW DATA ----");
 
             //log.Add(Environment.NewLine);
-            log.Add("View Data");
-            log.Add("Element ID, Name, Creator, Last Changed, Owner");
+            ModelData.Add("View Data");
+            ModelData.Add("Element ID, Name, Creator, Last Changed, Owner");
 
             FilteredElementCollector col = new FilteredElementCollector(doc)
              .OfClass(typeof(Autodesk.Revit.DB.View));
@@ -226,22 +259,154 @@ namespace File_Data_01
                 {
                     WorksharingTooltipInfo info = WorksharingUtils.GetWorksharingTooltipInfo(doc, e.Id);
 
-                    log.Add(e.Id + "," + e.Name + "," + info.Creator + "," + info.LastChangedBy + "," + info.Owner);
+                    ModelData.Add(e.Id + "," + e.Name + "," + info.Creator + "," + info.LastChangedBy + "," + info.Owner);
 
                     Debug.Print(string.Format("Name: {0}, Creator: {1}, Last Edited By: {2}, Owner: {3}",
                     e.Name, info.Creator, info.LastChangedBy, info.Owner));
                 }
                 catch (Exception ex)
                 {
-                    if (e == null) log.Add(ex.Message);
-                    else log.Add(e.Id + "," + ex.Message);
+                    if (e == null) ModelData.Add(ex.Message);
+                    else ModelData.Add(e.Id + "," + ex.Message);
                 }
                 //log.Add(Environment.NewLine);
             }
             //log.Add(Environment.NewLine);
             Debug.Print("---- VIEW DATA ----");
         }
-    
+
+
+        public void LevelData(Document doc)
+        {
+            if (doc == null) return;
+            Debug.Print("---- LEVEL DATA ----");
+            ModelData.Add("Level Data");
+            ModelData.Add("Element ID, Name, Creator, Last Changed, Owner");
+
+            FilteredElementCollector col = new FilteredElementCollector(doc)
+             .OfClass(typeof(Autodesk.Revit.DB.Level));
+
+            foreach (Element e in col)
+            {
+                try
+                {
+                    WorksharingTooltipInfo info = WorksharingUtils.GetWorksharingTooltipInfo(doc, e.Id);
+
+                    ModelData.Add(e.Id + "," + e.Name + "," + info.Creator + "," + info.LastChangedBy + "," + info.Owner);
+
+                    Debug.Print(string.Format("Name: {0}, Creator: {1}, Last Edited By: {2}, Owner: {3}",
+                    e.Name, info.Creator, info.LastChangedBy, info.Owner));
+                }
+                catch (Exception ex)
+                {
+                    if (e == null) ModelData.Add(ex.Message);
+                    else ModelData.Add(e.Id + "," + ex.Message);
+                }
+                //log.Add(Environment.NewLine);
+            }
+            //log.Add(Environment.NewLine);
+            Debug.Print("---- LEVEL DATA ----");
+        }
+
+
+        public void GridData(Document doc)
+        {
+            if (doc == null) return;
+            Debug.Print("---- GRID DATA ----");
+            ModelData.Add("Grid Data");
+            ModelData.Add("Element ID, Name, Creator, Last Changed, Owner");
+
+            FilteredElementCollector col = new FilteredElementCollector(doc)
+             .OfClass(typeof(Autodesk.Revit.DB.Grid));
+
+            foreach (Element e in col)
+            {
+                try
+                {
+                    WorksharingTooltipInfo info = WorksharingUtils.GetWorksharingTooltipInfo(doc, e.Id);
+
+                    ModelData.Add(e.Id + "," + e.Name + "," + info.Creator + "," + info.LastChangedBy + "," + info.Owner);
+
+                    Debug.Print(string.Format("Name: {0}, Creator: {1}, Last Edited By: {2}, Owner: {3}",
+                    e.Name, info.Creator, info.LastChangedBy, info.Owner));
+                }
+                catch (Exception ex)
+                {
+                    if (e == null) ModelData.Add(ex.Message);
+                    else ModelData.Add(e.Id + "," + ex.Message);
+                }
+                //log.Add(Environment.NewLine);
+            }
+            //log.Add(Environment.NewLine);
+            Debug.Print("---- GRID DATA ----");
+        }
+
+
+        public void SheetData(Document doc)
+        {
+            if (doc == null) return;
+            Debug.Print("---- SHEET DATA ----");
+            ModelData.Add("Sheet Data");
+            ModelData.Add("Element ID, Name, Creator, Last Changed, Owner");
+
+            FilteredElementCollector col = new FilteredElementCollector(doc)
+             .OfClass(typeof(Autodesk.Revit.DB.ViewSheet));
+
+            foreach (Element e in col)
+            {
+                try
+                {
+                    WorksharingTooltipInfo info = WorksharingUtils.GetWorksharingTooltipInfo(doc, e.Id);
+
+                    ModelData.Add(e.Id + "," + e.Name + "," + info.Creator + "," + info.LastChangedBy + "," + info.Owner);
+
+                    Debug.Print(string.Format("Name: {0}, Creator: {1}, Last Edited By: {2}, Owner: {3}",
+                    e.Name, info.Creator, info.LastChangedBy, info.Owner));
+                }
+                catch (Exception ex)
+                {
+                    if (e == null) ModelData.Add(ex.Message);
+                    else ModelData.Add(e.Id + "," + ex.Message);
+                }
+                //log.Add(Environment.NewLine);
+            }
+            //log.Add(Environment.NewLine);
+            Debug.Print("---- GRID DATA ----");
+        }
+
+
+        public void GenericElementData(Document doc, string elementType)
+        {
+            if (doc == null) return;
+            Debug.Print("---- {0} DATA ----", elementType.ToUpper());
+            ModelData.Add(string.Format("{0} Data", elementType));
+            ModelData.Add("Element ID, Name, Creator, Last Changed, Owner");
+
+            FilteredElementCollector col = new FilteredElementCollector(doc)
+             .OfClass(typeof(Autodesk.Revit.DB.Level));
+
+            foreach (Element e in col)
+            {
+                try
+                {
+                    WorksharingTooltipInfo info = WorksharingUtils.GetWorksharingTooltipInfo(doc, e.Id);
+
+                    ModelData.Add(e.Id + "," + e.Name + "," + info.Creator + "," + info.LastChangedBy + "," + info.Owner);
+
+                    Debug.Print(string.Format("Name: {0}, Creator: {1}, Last Edited By: {2}, Owner: {3}",
+                    e.Name, info.Creator, info.LastChangedBy, info.Owner));
+                }
+                catch (Exception ex)
+                {
+                    if (e == null) ModelData.Add(ex.Message);
+                    else ModelData.Add(e.Id + "," + ex.Message);
+                }
+                //log.Add(Environment.NewLine);
+            }
+            //log.Add(Environment.NewLine);
+            Debug.Print("---- {0} DATA ----", elementType.ToUpper());
+        }
+
 
         /// <summary>
         /// Format the log file name
@@ -254,6 +419,8 @@ namespace File_Data_01
             string date = DateTime.Now.ToString("yyMMdd");
             return string.Format("{0}_{1}_Element Data Log.csv", date, FileName);
         }
+
+        #endregion
     }
 }
 
