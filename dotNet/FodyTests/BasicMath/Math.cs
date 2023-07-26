0d8d6a077da83d04a4a64e5275ea35739e760ea2
CharlieP <charlie.portelli@gmail.com>
2023-07-26 15:17:22 -0400

    testing fody

---
+++ dotNet/FodyTests/BasicMath/Math.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BasicMath
+{
+    /// <summary>
+    /// This is a basic math class that compiles to a dll
+    /// this project is referenced into the FodyTests project
+    /// </summary>
+    public static class Math
+    {
+
+        public static double Add(double a, double b) { return a + b; }
+
+        public static double Subtract(double a, double b) { return a - b; }
+
+        public static double Multiply(double a, double b) { return a * b; }
+
+        public static double Divide(double a, double b) { return a / b; }
+    }
+}
---
+++ dotNet/FodyTests/FodyTests/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BasicMath;
+namespace FodyTests
+{
+    /// <summary>
+    /// This project references the BasicMath project
+    /// This project contains the Costura Fody nuget package
+    /// When the nuget package was not installed, the project would compile with the Basic math dll in the out put folder
+    /// Once the Fody package was installed, this project combiled with only the exe, and the masic math dll was embedded.
+    /// </summary>
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(BasicMath.Math.Add(12, 12));
+
+            
+
+
+            Console.WriteLine("---- DONE ----");
+            Console.ReadKey();
+        }
+    }
+}
