0e5881b6439d93f33fdce869ee65d4ba0f6b8e7f
CharlieP <charlie.portelli@gmail.com>
2023-07-26 15:16:36 -0400

    rhino common tests

---
+++ GH/RhinoCommonTests/RhinoCommonTests/Program.cs
@@ -0,0 +1,29 @@
+﻿using Rhino.Geometry;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RhinoCommonTests
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Point3d pt = CreatePoint();
+            Console.WriteLine(pt);
+
+            Console.WriteLine("---- DONE ----");
+            Console.ReadKey();
+        }
+
+
+        public static Point3d CreatePoint()
+        {
+            return new Point3d(3, 3, 3);
+        }
+
+
+    }
+}
