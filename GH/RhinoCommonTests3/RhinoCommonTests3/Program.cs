b533aa383c3c8e5168336bb62fbb7b0e96ab4d67
CharlieP <charlie.portelli@gmail.com>
2023-07-27 12:03:16 -0400

    adding rhino common test3

---
+++ GH/RhinoCommonTests3/RhinoCommonTests3/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Rhino;
+using Rhino.Geometry;
+
+namespace RhinoCommonTests3
+{
+
+    /// <summary>
+    /// in this project I installed fody Costura first then attempted to install the rhinocommon nuget package
+    /// 
+    /// </summary>
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Point3d pt = new Point3d(12, 34, 56);
+            Console.WriteLine(pt);
+
+            Console.WriteLine("---- DONE ----");
+            Console.ReadKey();
+        }
+    }
+}
