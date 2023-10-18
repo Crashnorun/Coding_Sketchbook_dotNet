39a7a68fd7ef55625d8eeb6e71b9bf54e8edd226
CharlieP <charlie.portelli@gmail.com>
2023-10-18 08:42:34 -0400

    singelton demo

---
+++ dotNet/Console Stuff/SingletonDemo/SingeltonDemo/ConsoleUI/Program.cs
@@ -0,0 +1,34 @@
+﻿namespace ConsoleUI
+{
+    internal class Program
+    {
+        static TableServers host1List = TableServers.GetTableServers();
+        static TableServers host2List = TableServers.GetTableServers();
+
+        static void Main(string[] args)
+        {
+            TableServers servers = TableServers.GetTableServers();
+
+            for (int i = 0; i < 5; i++)
+            {
+                //Console.WriteLine("The next server is: " + servers.GetNextServer());
+                Host1GetNextServer();
+                Host2GetNextServer();
+            }
+
+            Console.ReadLine();
+        }
+
+
+
+        private static void Host1GetNextServer()
+        {
+            Console.WriteLine(host1List.GetNextServer());
+        }
+
+        private static void Host2GetNextServer()
+        {
+            Console.WriteLine(host2List.GetNextServer());
+        }
+    }
+}
