0ac5eda414838cef851f4624b691171b09f6f403
CharlieP <charlie.portelli@gmail.com>
2023-11-17 15:42:53 -0500

    progress bar example

---
+++ dotNet/WPFProgressBars/WPFProgressBars/MainWindow.xaml.cs
@@ -0,0 +1,192 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.ComponentModel;
+
+namespace WPFProgressBars
+{
+    #region ---- NOTES ----
+
+    /*There are two examples of how to create a progress bar
+     */
+
+    #endregion
+
+
+
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window, INotifyPropertyChanged
+    {
+        #region ---- PROPERTIES ----
+
+        #region -- First Example --
+        private BackgroundWorker bgWorker = new BackgroundWorker();
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        private int workerState;
+
+        public int WorkerState
+        {
+            get { return workerState; }
+            set
+            {
+                workerState = value;
+                if (PropertyChanged != null) PropertyChanged(this, new PropertyChangedEventArgs("WorkerState"));
+            }
+        }
+
+        public string LabelValue
+        {
+            get { return labelvalue; }
+            set
+            {
+                labelvalue = value;
+                if (PropertyChanged != null) PropertyChanged(this, new PropertyChangedEventArgs("LabelValue"));
+            }
+        }
+
+        public string labelvalue;
+
+        #endregion
+
+
+        #region -- Second Example --
+
+        private BackgroundWorker bgworker2 = new BackgroundWorker();
+
+        public event PropertyChangedEventHandler PropertyChanged2;
+
+        private int workerState2;
+
+        public int WorkerState2
+        {
+            get { return workerState2; }
+            set
+            {
+                workerState2 = value;
+                if (PropertyChanged2 != null) PropertyChanged2(this, new PropertyChangedEventArgs("WorkerState2"));
+            }
+        }
+
+        public string LabelValue2
+        {
+            get { return labelvalue2; }
+            set
+            {
+                labelvalue2 = value;
+                if (PropertyChanged2 != null) PropertyChanged2(this, new PropertyChangedEventArgs("LabelValue"));
+            }
+        }
+
+        public string labelvalue2;
+
+        #endregion
+
+
+        #endregion
+
+
+        public MainWindow()
+        {
+            InitializeComponent();
+
+
+            SimpleExample ex1 = new SimpleExample();
+            ex1.Show();
+            ex1.pbar1.Value = 10;
+
+        }
+
+
+
+        #region ---- EVENTS ----
+
+        private void btnStart_Click(object sender, RoutedEventArgs e)
+        {
+            DataContext = this;
+            bgWorker.DoWork += (s, se) =>
+            {
+                for (int i = 0; i < 100; i++)
+                {
+                    System.Threading.Thread.Sleep(50);
+                    WorkerState = i;
+                    LabelValue = "Value: " + i;
+                }
+
+                MessageBox.Show("Work is done");
+
+            };
+            bgWorker.RunWorkerAsync();
+        }
+
+
+        private void btn_Click2(object sender, RoutedEventArgs e)
+        {
+            DataContext = this;
+            bgworker2.DoWork += (sender2, eventargs2) =>
+            {
+                lblMain2.Content = "Value: " + WorkerState2.ToString();
+            };
+            bgworker2.RunWorkerAsync();
+
+            bgworker2.WorkerReportsProgress = true;
+            bgworker2.DoWork += workerDoWork;
+            bgworker2.ProgressChanged += updateUI;
+            bgworker2.RunWorkerAsync();
+
+        }
+
+        private void workerDoWork(object sender, DoWorkEventArgs e)
+        {
+            int min = 0;
+            int max = 100;
+
+            for (int i = min; i < max; i++)
+            {
+                Thread.Sleep(50);
+                bgworker2.ReportProgress(i, "Current status:");
+            }
+        }
+
+
+        private void updateUI(object sender, ProgressChangedEventArgs e)
+        {
+            lblMain2.Content = e.ProgressPercentage;
+            pbarMain2.Value = e.ProgressPercentage;
+        }
+
+        #endregion
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            SimpleExample ex1 = new SimpleExample();
+            ex1.Show();
+
+
+            for (int i = 0; i < 100; i++)
+            {
+                Thread.Sleep(100);
+
+
+            }
+
+
+
+        }
+    }
+}
---
+++ dotNet/WPFProgressBars/WPFProgressBars/SimpleExample.xaml.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace WPFProgressBars
+{
+    /// <summary>
+    /// Interaction logic for SimpleExample.xaml
+    /// </summary>
+    public partial class SimpleExample : Window
+    {
+        public SimpleExample()
+        {
+            InitializeComponent();
+        }
+
+        private async void btnStart_Click(object sender, RoutedEventArgs e)
+        {
+            // Simulated data structure: List of Lists
+            List<List<int>> data = new List<List<int>>
+            {
+                new List<int> { 1, 2, 3 },
+                new List<int> { 4, 5, 6 },
+                new List<int> { 7, 8, 9 }
+            };
+
+            pbar1.Maximum = data.Count;
+            pbar2.Maximum = 100;
+            pbar2.Minimum = 0;
+
+
+            // Process the outer list
+            for (int i = 0; i < data.Count; i++)
+            {
+                // Update the global progress bar
+                pbar1.Value = i + 1;
+                lbl1.Content = i + 1;
+
+                // Process the inner list
+                for (int j = 0; j < data[i].Count; j++)
+                {
+                    // Simulated processing delay
+                    await Task.Delay(200);
+
+                    // Update the granular progress bar
+                    pbar2.Value = (j + 1) * (100.0 / data[i].Count);
+                    lbl2.Content = (j + 1) * (100.0 / data[i].Count);
+                }
+
+                // Reset the granular progress bar for the next outer list item
+                pbar2.Value = 0;
+            }
+
+            // Reset the global progress bar
+            pbar1.Value = 0;
+
+            MessageBox.Show("Processing Complete!");
+        }
+
+        // do stuff here
+
+
+
+    }
+}
